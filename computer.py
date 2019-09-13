87f3e300a8e0f7aebd2ab05e307393733d2ac08d
ravizhan <ravizhan@hotmail.com>
2019-09-13 09:33:56 +0800

    修复bug 完成单位换算

--- computer.py
+++ computer.py
@@ -41,6 +41,8 @@ print('''      欢迎使用 单位换算/计算器
   项目地址：https://github.com/jim-ravi/Unit-conversion-calculator
         made by Ravi&Jim''')
 runtime = 1
+check = 0
+wrong = 0
 while runtime == 1:
     choose = input('''功能：1、单位换算  2、面积计算  3、周长计算
     请输入要进行的操作(序号)：''')
@@ -69,7 +71,7 @@ while runtime == 1:
         # 单位换算
         if int(choose) == 1:
             change_choose = int(input('''1、面积转换 2、长度转换 3、速度转换 4、体积转换 5、重量转换
-                                                    请输入：'''))
+                                         请输入：'''))
             # 面积转换
             if change_choose == 1:
                 area_change_choose = int(input('''
@@ -283,18 +285,177 @@ while runtime == 1:
 
             # 速度转换
             if change_choose == 3:
-                print('''功能开发中，敬请期待''')
-                break
+                area_change_choose = int(input('''1、米/秒-千米/秒
+                                                  2、米/秒-千米/时
+                                                  3、千米/秒-千米/时
+                                                  请输入：'''))
+                # 米/秒-千米/秒
+                if area_change_choose == 1:
+                    area_choose = int(input('''1、米/秒-千米/秒
+                                               2、千米/秒-米/秒
+                                               请输入选择：'''))
+                    if area_choose == 1:
+                        change_num = float(input('''请输入要转换的数值(单位:米/秒):'''))
+                        print(str(change_num) + '米/秒=' + str(change.speed.ms_kms.ms2kms(change_num)) + '千米/秒')
+                    elif area_choose == 2:
+                        change_num = float(input('''请输入要转换的数值(单位:千米/秒):'''))
+                        print(str(change_num) + '千米/秒=' + str(change.speed.ms_kms.kms2ms(change_num)) + '米/秒')
+                    run = int(input('''继续计算请按1
+                                        退出请按2'''))
+                    if run == 1:
+                        continue
+                    else:
+                        break
+                # 米/秒-千米/时
+                if area_change_choose == 2:
+                    area_choose = int(input('''1、米/秒-千米/时
+                                               2、千米/时-米/秒
+                                               请输入选择：'''))
+                    if area_choose == 1:
+                        change_num = float(input('''请输入要转换的数值(单位:米/秒):'''))
+                        print(str(change_num) + '米/秒=' + str(change.speed.ms_kmh.ms2kmh(change_num)) + '千米/时')
+                    elif area_choose == 2:
+                        change_num = float(input('''请输入要转换的数值(单位:千米/时):'''))
+                        print(str(change_num) + '千米/时=' + str(change.speed.ms_kmh.kmh2ms(change_num)) + '米/秒')
+                    run = int(input('''继续计算请按1
+                                       退出请按2'''))
+                    if run == 1:
+                        continue
+                    else:
+                        break
+                # 千米/秒-千米/时
+                if area_change_choose == 3:
+                    area_choose = int(input('''1、千米/秒-千米/时
+                                               2、千米/时-千米/秒
+                                               请输入选择：'''))
+                    if area_choose == 1:
+                        change_num = float(input('''请输入要转换的数值(单位:千米/秒):'''))
+                        print(str(change_num) + '千米/秒=' + str(change.speed.kms_kmh.kms2kmh(change_num)) + '千米/时')
+                    elif area_choose == 2:
+                        change_num = float(input('''请输入要转换的数值(单位:千米/时):'''))
+                        print(str(change_num) + '千米/时=' + str(change.speed.kms_kmh.kmh2kms(change_num)) + '千米/秒')
+                    run = int(input('''继续计算请按1
+                                       退出请按2'''))
+                    if run == 1:
+                        continue
+                    else:
+                        break
 
             # 体积转换
             if change_choose == 4:
-                print('''功能开发中，敬请期待''')
-                break
+                area_change_choose = int(input('''1、立方毫米-立方厘米
+                                                  2、立方厘米-立方分米
+                                                  3、立方分米-立方米
+                                                  请输入：'''))
+                # 立方毫米-立方厘米
+                if area_change_choose == 1:
+                    area_choose = int(input('''1、立方毫米-立方厘米
+                                               2、立方厘米-立方毫米
+                                               请输入选择：'''))
+                    if area_choose == 1:
+                        change_num = float(input('''请输入要转换的数值(单位:立方毫米):'''))
+                        print(str(change_num) + '立方毫米=' + str(change.volume.mm3_cm3.mm32cm3(change_num)) + '立方厘米')
+                    elif area_choose == 2:
+                        change_num = float(input('''请输入要转换的数值(单位:立方厘米):'''))
+                        print(str(change_num) + '立方厘米=' + str(change.volume.mm3_cm3.cm32mm3(change_num)) + '立方毫米')
+                    run = int(input('''继续计算请按1
+                                        退出请按2'''))
+                    if run == 1:
+                        continue
+                    else:
+                        break
+                # 立方厘米-立方分米
+                if area_change_choose == 2:
+                    area_choose = int(input('''1、立方厘米-立方分米
+                                               2、立方分米-立方厘米
+                                               请输入选择：'''))
+                    if area_choose == 1:
+                        change_num = float(input('''请输入要转换的数值(单位:立方厘米):'''))
+                        print(str(change_num) + '立方厘米=' + str(change.volume.cm3_dm3.cm32dm3(change_num)) + '立方分米')
+                    elif area_choose == 2:
+                        change_num = float(input('''请输入要转换的数值(单位:立方分米):'''))
+                        print(str(change_num) + '立方分米=' + str(change.volume.cm3_dm3.dm32cm3(change_num)) + '立方厘米')
+                    run = int(input('''继续计算请按1
+                                       退出请按2'''))
+                    if run == 1:
+                        continue
+                    else:
+                        break
+                # 立方分米-立方米
+                if area_change_choose == 3:
+                    area_choose = int(input('''1、立方分米-立方米
+                                               2、立方米-立方分米
+                                               请输入选择：'''))
+                    if area_choose == 1:
+                        change_num = float(input('''请输入要转换的数值(单位:立方分米):'''))
+                        print(str(change_num) + '立方分米=' + str(change.volume.dm3_m3.dm32m3(change_num)) + '立方米')
+                    elif area_choose == 2:
+                        change_num = float(input('''请输入要转换的数值(单位:立方米):'''))
+                        print(str(change_num) + '立方米=' + str(change.volume.dm3_m3.m32dm3(change_num)) + '立方分米')
+                    run = int(input('''继续计算请按1
+                                       退出请按2'''))
+                    if run == 1:
+                        continue
+                    else:
+                        break
 
             # 重量转换
             if change_choose == 5:
-                print('''功能开发中，敬请期待''')
-                break
+                area_change_choose = int(input('''1、毫克-克
+                                                  2、克-千克
+                                                  3、千克-吨
+                                                  请输入：'''))
+                # 毫克-克
+                if area_change_choose == 1:
+                    area_choose = int(input('''1、毫克-克
+                                               2、克-毫克
+                                               请输入选择：'''))
+                    if area_choose == 1:
+                        change_num = float(input('''请输入要转换的数值(单位:毫克):'''))
+                        print(str(change_num) + '毫克=' + str(change.weight.mg_g.mg2g(change_num)) + '克')
+                    elif area_choose == 2:
+                        change_num = float(input('''请输入要转换的数值(单位:克):'''))
+                        print(str(change_num) + '克=' + str(change.weight.mg_g.g2mg(change_num)) + '毫克')
+                    run = int(input('''继续计算请按1
+                                        退出请按2'''))
+                    if run == 1:
+                        continue
+                    else:
+                        break
+                # 克-千克
+                if area_change_choose == 2:
+                    area_choose = int(input('''1、克-千克
+                                               2、千克-克
+                                               请输入选择：'''))
+                    if area_choose == 1:
+                        change_num = float(input('''请输入要转换的数值(单位:克):'''))
+                        print(str(change_num) + '克=' + str(change.weight.g_kg.g2kg(change_num)) + '千克')
+                    elif area_choose == 2:
+                        change_num = float(input('''请输入要转换的数值(单位:千克):'''))
+                        print(str(change_num) + '千克=' + str(change.weight.g_kg.kg2g(change_num)) + '克')
+                    run = int(input('''继续计算请按1
+                                       退出请按2'''))
+                    if run == 1:
+                        continue
+                    else:
+                        break
+                # 千克-吨
+                if area_change_choose == 3:
+                    area_choose = int(input('''1、千克-吨
+                                               2、吨-千克
+                                               请输入选择：'''))
+                    if area_choose == 1:
+                        change_num = float(input('''请输入要转换的数值(单位:千克):'''))
+                        print(str(change_num) + '千克=' + str(change.weight.kg_t.kg2t(change_num)) + '吨')
+                    elif area_choose == 2:
+                        change_num = float(input('''请输入要转换的数值(单位:吨):'''))
+                        print(str(change_num) + '吨=' + str(change.weight.kg_t.t2kg(change_num)) + '千克')
+                    run = int(input('''继续计算请按1
+                                       退出请按2'''))
+                    if run == 1:
+                        continue
+                    else:
+                        break
 
         # 面积计算
         elif int(choose) == 2:
